b1ec06bfba65f5cd0581af9a2d4c60b391d58008
giobaldac <giorgio.baldaccini@contentwise.tv>
2015-09-30 10:30:23 +0200

    Initial release

---
+++ README.txt
@@ -0,0 +1,2 @@
+This plugin shows the parameters used for a build in inside the Build History Widget
+
---
+++ src/main/java/jenkins/plugins/build_history_parameters/BuildHistoryParametersBuildAction.java
@@ -0,0 +1,59 @@
+package jenkins.plugins.build_history_parameters;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.InvisibleAction;
+import hudson.model.ParameterValue;
+import hudson.model.ParametersAction;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Adds parameters used for build (if any) to displayName and description
+ * of new builds.  It will have no effect on builds generated before this
+ * plugin was added.
+ * 
+ * @author Giorgio Baldaccini
+ */
+public class BuildHistoryParametersBuildAction extends InvisibleAction {
+    private AbstractBuild<?,?> build;
+
+    public BuildHistoryParametersBuildAction(AbstractBuild<?,?> build) {
+        this.build = build;
+    }
+
+    @Extension
+    public static final class RunListenerImpl extends RunListener<AbstractBuild<?,?>> {
+
+        @Override
+        public void onStarted(AbstractBuild<?, ?> r, TaskListener listener) {
+            try {
+                ParametersAction parametersAction = r.getAction(ParametersAction.class);
+                if (parametersAction != null) {
+                    List<ParameterValue> params = parametersAction.getParameters();
+                    String newDescription = "";
+                    String newDisplayName = "";
+                    for (ParameterValue currParam: params) {
+                        if (newDisplayName.length() == 0) {
+                            newDisplayName = currParam.getValue().toString();
+                        }
+                        else {
+                            newDescription += " ";
+                        }
+                        
+                        newDescription += currParam.getName()+"="+currParam.getValue();
+                    }
+                    if (newDescription.length() > 0)
+                        r.setDescription(newDescription);
+                    
+                    if (newDisplayName.length() > 0)
+                        r.setDisplayName(newDisplayName);
+                }
+            }
+            catch(Exception ex) {
+            }
+        }
+    }
+}
